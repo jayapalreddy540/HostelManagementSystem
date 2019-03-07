76b7001010f5cf7804e069ce6981d3471c521401
jayapalreddy540 <jayapalreddymjpal@gmail.com>
2019-03-07 07:49:41 +0530

    preview of rooms selection

--- app/src/main/java/tk/codme/hostelmanagementsystem/RoomFragment.java
+++ app/src/main/java/tk/codme/hostelmanagementsystem/RoomFragment.java
@@ -1,6 +1,9 @@
 package tk.codme.hostelmanagementsystem;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +16,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,8 +45,57 @@ public class RoomFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMainView = inflater.inflate(R.layout.activity_roomfrag, container, false);
 
-        FirebaseUser current_user= FirebaseAuth.getInstance().getCurrentUser();
-        final String uid=current_user.getUid();
+        FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+        final String uid = current_user.getUid();
+
+        SharedPreferences sp=this.getActivity().getSharedPreferences("tk.codme.hostelmanagementsystem", Context.MODE_PRIVATE);
+        final String designation=sp.getString("designation","");
+        final String caretaker=sp.getString("caretaker","");
+
+        for(int i=0;i<=15;i++){
+            chk[i]=(CheckBox) mMainView.findViewById(mRoom[i]);
+        }
+        if(designation.equals("student")) {
+            mRef = FirebaseDatabase.getInstance().getReference().child("users").child("wardens").child(caretaker).child("seats");
+        }
+        else if(designation.equals("admin")){
+            mRef = (DatabaseReference) FirebaseDatabase.getInstance().getReference().child("users").child("wardens").orderByChild("caretaker").equalTo(uid);
+            mRef.child("seats");
+        }
+        else{
+            mRef = FirebaseDatabase.getInstance().getReference().child("users").child("wardens").child(uid).child("seats");
+        }
+
+        mRef.keepSynced(true);
+        try{
+            mRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    try {
+                        for (int i = 1; i <= 15; i++) {
+                            alloc[i] = dataSnapshot.child("s" + i).getValue().toString();
+                            Log.d(i + "", alloc[i]);
+                            if (alloc[i].equals("true")) {
+                                if (chk[i].isChecked()) {
+
+                                } else {
+                                    chk[i].setChecked(true);
+                                }
+                            }
+                        }
+                    }
+                    catch(Exception e){}
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+        catch(Exception e){}
+        //FirebaseUser current_user= FirebaseAuth.getInstance().getCurrentUser();
+        //final String uid=current_user.getUid();
         mRef= FirebaseDatabase.getInstance().getReference().child("users").child("wardens").child(uid).child("seats");
         for (int i = 1; i <=15; i++) {
             chk[i] = (CheckBox) mMainView.findViewById(mRoom[i]);
